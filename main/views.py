53aa080556ce9ec7017033c83544083bb8b1c504
Matvey1212 <matkhusainov@icloud.com>
2026-04-04 06:48:02 +0500

    Сделал вьюшку профиля

--- main/views.py
+++ main/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
+from main.models import Item
+
+
 # Create your views here.
+def prof(request,username):
+    items=Item.objects.filter(author__username=username)
+    return render(request,"profile.html",{"items":items,"username":username})
+
--- wishlist/urls.py
+++ wishlist/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from main.views import prof, register_view
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("profile/<str:username>/",prof),
+    path("register",register_view),
 ]
