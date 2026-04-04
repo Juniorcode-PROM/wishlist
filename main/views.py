2ec98605390980b3dfd709e5e4f807a979a006cf
Matvey1212 <matkhusainov@icloud.com>
2026-04-04 07:04:08 +0500

    Сделал вьюшку профиля

--- main/views.py
+++ main/views.py
@@ -1,8 +1,9 @@
 from django.contrib.auth.models import User
+from django.db.transaction import commit
 from django.shortcuts import render, redirect
 
-from main.forms import RegisterForm
-
+from main.forms import RegisterForm, Add_Vishlist
+from main.models import Item
 
 def register_view(request):
     if request.method == "POST":
@@ -16,3 +17,17 @@ def register_view(request):
     else:
         form = RegisterForm()
     return render(request, "register.html", {"form": form})
+def create_vish(request):
+    if request.method == "POST":
+        form = Add_Vishlist(request.POST, request.FILES)
+        if form.is_valid():
+            vish = Item.object.create(
+                name=form.cleaned_data["predmet"],
+                author=request.user
+            )
+            return redirect(f"/profile/{request.user.username}/")
+    else:
+        form = Add_Vishlist()
+    return render(request, "create_vish.html", {
+        "form": form
+    })
--- wishlist/urls.py
+++ wishlist/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from main.views import create_vish
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("create_wish", create_vish),
 ]
