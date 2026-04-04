87e9b795364eca476ea4d4f2d1235946b0ede8f2
Matvey1212 <matkhusainov@icloud.com>
2026-04-04 06:19:58 +0500

    Сделал вьюшку регистрации

--- main/views.py
+++ main/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.shortcuts import render, redirect
 
-# Create your views here.
+from main.forms import RegisterForm
+
+
+def register_view(request):
+    if request.method == "POST":
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            User.objects.create_user(
+                username=form.cleaned_data["login"],
+                password=form.cleaned_data["password"]
+            )
+            return redirect("/login")
+    else:
+        form = RegisterForm()
+    return render(request, "register.html", {"form": form})
